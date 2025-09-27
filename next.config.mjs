7cf7414777e73a82edcef4bfb8ed6e1b08e7af9a
Florian <dark.joosh@gmail.com>
2025-09-27 12:01:26 +0200

    Revert "connect to supabase"
    This reverts commit 80a8534499ff468287a19bbcfbfc2f5649a4ef24.
    
    # Conflicts:
    #	app/_lib/data-service.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      new URL(
-        `${process.env.SUPABASE_URL}/storage/v1/object/public/cabin-images/**`,
-      ),
-    ],
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
